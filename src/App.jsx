501431c5ca0d700403e9ee98d32b4570424496ee
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-19 17:42:31 +0100

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Route, Router } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
+    <div>
+      <Router>
+      <Route path="/" element={<RootLayout />} >
+       
+        </Route>
+      </Router>
      
-    </>
+    </div>
   )
 }
 
