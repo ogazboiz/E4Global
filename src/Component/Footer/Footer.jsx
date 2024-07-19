d52010e879b9c8307d4ecf6f13f34f5f1388d29b
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-19 19:59:00 +0100

    ee

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,9 +1,153 @@
-
-
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="relative bg-white">
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: "url('/assets/Images/footer.jpg')" }}
+      ></div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-[#FE9F11] opacity-50"></div>
+
+      {/* Content */}
+      <div className="relative max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8">
+        <div className="flex flex-col mt-16 lg:flex-row lg:justify-between lg:items-center">
+          {/* Centered Content */}
+          <div>
+            
+          </div>
+          <div className="flex flex-col items-center flex-1 lg:items-start">
+            <div className="mb-8 text-center lg:text-left lg:mb-0">
+          
+              <ul className="mt-6 space-y-1">
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    Service
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    FAQs
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center flex-1 lg:items-start">
+            <div className="mb-8 text-center lg:text-left lg:mb-0">
+            
+              <ul className="mt-6 space-y-1">
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    Terms of Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    Cookie Policy
+                  </a>
+                </li>
+              
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center flex-1 lg:items-start">
+            <div className="mb-8 text-center lg:text-left lg:mb-0">
+              <strong className="font-medium text-white">Follow Our Socials:</strong>
+              <ul className="flex gap-6 mt-6 space-y-1">
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    <FaFacebook className="text-4xl text-blue-500" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    <FaInstagram className="text-4xl text-blue-500" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200"
+                    href="#"
+                  >
+                    <FaTwitter className="text-4xl text-blue-500" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="pt-8 mt-16 border-t border-gray-100">
+          <p className="text-center text-gray-300 text-xs/relaxed">
+            © Company 2024. All rights reserved.
+            <br />
+            Created with
+            <a
+              href="#"
+              className="text-white underline transition hover:text-gray-200"
+            >
+              Laravel
+            </a>
+            and
+            <a
+              href="#"
+              className="text-white underline transition hover:text-gray-200"
+            >
+              Laravel Livewire
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,80 +1,36 @@
-
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header className="bg-white">
-  <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-    <a className="block text-teal-600" href="#">
-      <span className="sr-only">Home</span>
-      
-    </a>
-
-    <div className="flex flex-1 items-center justify-end md:justify-between">
-      <nav aria-label="Global" className="hidden md:block">
-        <ul className="flex items-center gap-6 text-sm">
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About </a>
-          </li>
-
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Careers </a>
-          </li>
-
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> History </a>
-          </li>
-
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Services </a>
-          </li>
-
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Projects </a>
-          </li>
-
-          <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Blog </a>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <div className="flex items-center gap-4">
-        <div className="sm:flex sm:gap-4">
-          <a
-            className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-            href="#"
-          >
-            Login
-          </a>
-
-          <a
-            className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-            href="#"
-          >
-            Register
-          </a>
+    <header className="items-center px-3 py-2 bg-white rounded-full md:m-10 md:flex md:justify-between">
+     <Link to='/'>
+        <div className="bg-[#FE9F11] h-[55px] w-[65px] rounded-full flex items-center justify-center">
+          <img src="/assets/Images/logo.png" alt="logo" className="flex" style={{ width: "100px" }}/>
         </div>
+      </Link>
 
-        <button
-          className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-        >
-          <span className="sr-only">Toggle menu</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div> */}
-    </div>
+  <div className="items-center justify-between hidden w-3/5 md:flex">
+    <nav>
+      <ul className="flex space-x-10 justify-starts">
+        <li>
+          <NavLink to="/" className="font-bold transition-all hover:underline hover:text-slate-900 hover:"> Services </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="font-bold transition-all hover:underline hover:text-slate-400 hover:">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services" className="font-bold transition-all hover:underline hover:text-slate-400 hover:">Contact Us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product" className="font-bold transition-all hover:underline hover:text-slate-400 hover:">FAQs</NavLink>
+        </li>
+      </ul>
+    </nav>
+   
   </div>
 </header>
-  )
+
+  );
 }
 
-export default Navbar
+export default Navbar;
