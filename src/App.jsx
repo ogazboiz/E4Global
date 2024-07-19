0089a590f57c03f790b6b135e9f68e97910b659c
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-19 17:48:31 +0100

    dw

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 
 import { Route, Router } from 'react-router-dom'
 import './App.css'
-import Body from './components/Body'
+
 
 function App() {
   
 
   return (
     <div>
-      <Router>
-      <Route path="/" element={<RootLayout />} >
-       
-        </Route>
-      </Router>
+     
      
     </div>
   )
