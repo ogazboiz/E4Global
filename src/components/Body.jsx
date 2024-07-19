370d6dcfbe60e133b35ad9a04cacb3f59159b44b
ogazboiz <ogazboizakpolo@gmail.com>
2024-07-19 17:41:39 +0100

    created the body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div>
+        <p>header</p>
+        <p>our services</p>
+        <p>what our users say</p>
+        <p>track you package</p>
+        
+    </div>
+  )
+}
+
+export default Body
