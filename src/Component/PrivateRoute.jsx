36e8fb6f7e5052981bf3546aa5239f0859d98c4a
ogazboiz <ogazboizakpolo@gmail.com>
2024-07-20 21:17:43 +0100

    fixing the admin routing

--- src/Component/PrivateRoute.jsx
+++ src/Component/PrivateRoute.jsx
@@ -3,12 +3,14 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  if (!user) {
-    return <Navigate to="/adminlogin" />;
+  if (!isAuthenticated) {
+    // If the user is not authenticated, redirect to the login page
+    return <Navigate to="/admin/login" />;
   }
 
+  // If the user is authenticated, render the children components
   return children;
 };
 
--- src/Layout/AdminLayout.jsx
+++ src/Layout/AdminLayout.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Component/Sidebar';
 import Header from "../Component/Navbar/Header";
+
 function AdminLayout() {
   return (
     <>
-    <Header/>
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1">
-        
-        <main className="p-10">
-          <Outlet />
-        </main>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <main className="p-10">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
     </>
   );
 }
