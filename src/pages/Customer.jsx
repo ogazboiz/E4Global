aace8e700069f432db3e37cd02f3bff021ea7d62
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-25 01:11:23 +0100

    yes

--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,14 +1,9 @@
-
-
 import CustomerTable from "../Component/Customer/Table/CustomerTable";
-
 
 function Customer({ customers }) {
   return (
     <div className="flex flex-col p-4 space-y-4">
-     
       <CustomerTable customers={customers} />
-      
     </div>
   );
 }
